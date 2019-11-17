993c8295a821be8119283cf31a4f7c67f4daa265
Prateek057 <bagrecha.prateek@gmail.com>
2019-11-17 02:02:52 +0100

    Add tags to tales

--- src/main/java/org/wildtales/wildtalesspringapp/model/Chapter.java
+++ src/main/java/org/wildtales/wildtalesspringapp/model/Chapter.java
@@ -4,8 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -13,6 +11,6 @@ public class Chapter {
     private String title;
     private int order;
     private String story;
-    private List<String> imageUrls;
-    private List<String> videoUrls;
+    private String imageUrl;
+    private String videoUrl;
 }
